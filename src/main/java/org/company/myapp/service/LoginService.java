4f0d1df64104ee9f92d5e6b79f185dc4ae8a6b5d
Minho Ji <59356172+minho4418@users.noreply.github.com>
2022-07-07 15:24:13 +0900

    Delete src directory

--- src/main/java/org/company/myapp/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package org.company.myapp.service;
-
-import javax.servlet.http.HttpSession;
-
-import org.company.myapp.advice.ErrorCode;
-
-public interface LoginService {
-	ErrorCode loginCheck(String email, String passwd, HttpSession session);
-}
